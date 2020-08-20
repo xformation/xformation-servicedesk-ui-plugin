f9202fe9d653ec726832c257e1b9f69b3b01e612
dipti96 <diptippatel96@gmail.com>
2020-08-20 13:04:18 +0530

    add dynamic content and chart in tickets page

--- src/domain/Tickets/index.tsx
+++ src/domain/Tickets/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Breadcrumbs } from '../../components/Breadcrumbs';
+import { Bar } from 'react-chartjs-2';
 
 export class Tickets extends React.Component<any, any> {
     breadCrumbs: any;
@@ -115,6 +116,21 @@ export class Tickets extends React.Component<any, any> {
         this.calculateTotalPages(this.state.TicketsData);
     };
 
+    barChartData = {
+        labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '01', '02', '03', '04', '05', '06', '07', '08', '09', 10],
+        datasets: [
+            {
+                label: '',
+                backgroundColor: 'rgba(222, 233, 249, 1)',
+                borderColor: 'rgba(222, 233, 249, 1)',
+                borderWidth: 1,
+                hoverBackgroundColor: 'rgba(222, 233, 249, 1)',
+                hoverBorderColor: 'rgba(222, 233, 249, 1)',
+                data: [5, 10, 12, 15, 20, 4, 10, 13, 17, 16, 20, 22, 13, 17, 15, 14, 16, 18, 16, 17, 12, 22, 30, 21, 5, 10, 12, 15, 20, 4, 10, 13, 17, 16, 20, 22, 13, 17, 15, 14]
+            }
+        ]
+    };
+
     calculateTotalPages = (displayData: any) => {
         const { perPageLimit } = this.state;
         let indexOfLastData = Math.ceil(displayData.length / perPageLimit);
@@ -219,14 +235,62 @@ export class Tickets extends React.Component<any, any> {
                             </div>
                         </div>
                     </div>
+                    <div className="row m-t-1">
+                        <div className="col-lg-8 col-md-8 col-sm-12">
+                            <div className="page-heading">
+                                <h1>2450</h1>
+                                <span>Total No.of Tickets</span>
+                            </div>
+                        </div>
+                        <div className="col-lg-2 col-md-2 col-sm-12 text-right">
+                            <div className="form-group">
+                                <select className="form-control" id="Quarter">
+                                    <option value="" selected>Select Quarter</option>
+                                    <option value="Quater1">Quarter 1</option>
+                                    <option value="Quater2">Quarter 2</option>
+                                    <option value="Quater3">Quarter 3</option>
+                                    <option value="Quater4">Quarter 4</option>
+                                    <option value="CustomRange">Custom Range</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="d-block chart-inner">
+                        <Bar
+                            data={this.barChartData}
+                            width={100}
+                            height={100}
+                            options={{
+                                maintainAspectRatio: false,
+                                legend: {
+                                    display: false,
+                                    position: 'right'
+                                },
+                                scales: {
+                                    yAxes: [{
+                                        ticks: {
+                                            fontColor: '#fff',
+                                            fontSize: 12
+                                        },
+                                    }],
+                                    xAxes: [{
+                                        ticks: {
+                                            fontColor: 'rgba(169, 185, 198, 1)',
+                                            fontSize: 12
+                                        },
+                                    }]
+                                }
+                            }}
+                        />
+                    </div>
                     <div className="common-container border-bottom-0">
                         <div className="row">
                             <div className="col-xl-2 col-lg-2 col-md-2 col-sm-12">
                                 <div className="form-group">
-                                    <label htmlFor="rousourceGroup">
+                                    <label htmlFor="Agents">
                                         Agents&nbsp;&nbsp;&nbsp;
                                     </label>
-                                    <select className="form-control" id="rousourceGroup">
+                                    <select className="form-control" id="Agents">
                                         <option value="" selected>Select Agent</option>
                                         <option value="JeremyGriffin">Jeremy Griffin</option>
                                         <option value="TimothyDean">Timothy Dean</option>
@@ -237,10 +301,10 @@ export class Tickets extends React.Component<any, any> {
                             </div>
                             <div className="col-xl-2 col-lg-2 col-md-2 col-sm-12">
                                 <div className="form-group">
-                                    <label htmlFor="resources">
+                                    <label htmlFor="Groups">
                                         Groups&nbsp;&nbsp;&nbsp;
                                     </label>
-                                    <select className="form-control" id="resources">
+                                    <select className="form-control" id="Groups">
                                         <option value="" selected>Select Groups</option>
                                         <option value="Billings">Billings</option>
                                         <option value="Escalation">Escalation</option>
@@ -251,10 +315,10 @@ export class Tickets extends React.Component<any, any> {
                             </div>
                             <div className="col-xl-2 col-lg-2 col-md-2 col-sm-12">
                                 <div className="form-group">
-                                    <label htmlFor="timeRange">
+                                    <label htmlFor="Created">
                                         Created&nbsp;&nbsp;&nbsp;
                                     </label>
-                                    <select className="form-control" id="timeRange">
+                                    <select className="form-control" id="Created">
                                         <option value="" selected>Select Created</option>
                                         <option value="AnyTime">Any Time</option>
                                         <option value="Whithin30">Whithin 30</option>
@@ -265,10 +329,10 @@ export class Tickets extends React.Component<any, any> {
                             </div>
                             <div className="col-xl-2 col-lg-2 col-md-2 col-sm-12">
                                 <div className="form-group">
-                                    <label htmlFor="timeRange">
+                                    <label htmlFor="dueby">
                                         Due by&nbsp;&nbsp;&nbsp;
                                     </label>
-                                    <select className="form-control" id="timeRange">
+                                    <select className="form-control" id="dueby">
                                         <option value="" selected>Select Due by</option>
                                         <option value="overdue">Overdue</option>
                                         <option value="today">Today</option>
@@ -279,10 +343,10 @@ export class Tickets extends React.Component<any, any> {
                             </div>
                             <div className="col-xl-2 col-lg-2 col-md-2 col-sm-12">
                                 <div className="form-group">
-                                    <label htmlFor="timeRange">
+                                    <label htmlFor="Status">
                                         Status&nbsp;&nbsp;&nbsp;
                                     </label>
-                                    <select className="form-control" id="timeRange">
+                                    <select className="form-control" id="Status">
                                         <option value="" selected>Select Status</option>
                                         <option value="AllUnresolved">All Unresolved</option>
                                         <option value="Open">Open</option>
@@ -293,10 +357,10 @@ export class Tickets extends React.Component<any, any> {
                             </div>
                             <div className="col-xl-2 col-lg-2 col-md-2 col-sm-12">
                                 <div className="form-group">
-                                    <label htmlFor="timeRange">
+                                    <label htmlFor="Priority">
                                         Priority&nbsp;&nbsp;&nbsp;
                                     </label>
-                                    <select className="form-control" id="timeRange">
+                                    <select className="form-control" id="Priority">
                                         <option value="" selected>Select Priority</option>
                                         <option value="Low">Low</option>
                                         <option value="Medium">Medium</option>
@@ -307,10 +371,10 @@ export class Tickets extends React.Component<any, any> {
                             </div>
                             <div className="col-xl-2 col-lg-2 col-md-2 col-sm-12">
                                 <div className="form-group">
-                                    <label htmlFor="timeRange">
+                                    <label htmlFor="Type">
                                         Type&nbsp;&nbsp;&nbsp;
                                     </label>
-                                    <select className="form-control" id="timeRange">
+                                    <select className="form-control" id="Type">
                                         <option value="" selected>Select Type</option>
                                         <option value="None">None</option>
                                         <option value="Question">Question</option>
@@ -320,10 +384,10 @@ export class Tickets extends React.Component<any, any> {
                             </div>
                             <div className="col-xl-2 col-lg-2 col-md-2 col-sm-12">
                                 <div className="form-group">
-                                    <label htmlFor="timeRange">
+                                    <label htmlFor="Source">
                                         Source&nbsp;&nbsp;&nbsp;
                                     </label>
-                                    <select className="form-control" id="timeRange">
+                                    <select className="form-control" id="Source">
                                         <option value="" selected>Select Source</option>
                                         <option value="Email">Email</option>
                                         <option value="Portal">Portal</option>
@@ -333,10 +397,10 @@ export class Tickets extends React.Component<any, any> {
                             </div>
                             <div className="col-xl-2 col-lg-2 col-md-2 col-sm-12">
                                 <div className="form-group">
-                                    <label htmlFor="timeRange">
+                                    <label htmlFor="Tags">
                                         Tags&nbsp;&nbsp;&nbsp;
                                     </label>
-                                    <select className="form-control" id="timeRange">
+                                    <select className="form-control" id="Tags">
                                         <option value="" selected>Select Tags</option>
                                         <option value="CPU">CPU</option>
                                         <option value="Memory">Memory</option>
@@ -345,10 +409,10 @@ export class Tickets extends React.Component<any, any> {
                             </div>
                             <div className="col-xl-2 col-lg-2 col-md-2 col-sm-12">
                                 <div className="form-group">
-                                    <label htmlFor="timeRange">
+                                    <label htmlFor="Companies">
                                         Companies&nbsp;&nbsp;&nbsp;
                                     </label>
-                                    <select className="form-control" id="timeRange">
+                                    <select className="form-control" id="Companies">
                                         <option value="" selected>Select Company</option>
                                         <option value="CompanyName1">Company Name 1</option>
                                         <option value="CompanyName2">Company Name 2</option>
@@ -357,10 +421,10 @@ export class Tickets extends React.Component<any, any> {
                             </div>
                             <div className="col-xl-2 col-lg-2 col-md-2 col-sm-12">
                                 <div className="form-group">
-                                    <label htmlFor="timeRange">
+                                    <label htmlFor="Contacts">
                                         Contacts&nbsp;&nbsp;&nbsp;
                                     </label>
-                                    <select className="form-control" id="timeRange">
+                                    <select className="form-control" id="Contacts">
                                         <option value="" selected>Select Contacts</option>
                                         <option value="Any">Any</option>
                                         <option value="ListOfContacts">List of Contacts</option>
