ec855ad5aa2def59e1af442a760cf0e709233ed8
Satish pal <satishweb93@gmail.com>
2020-08-27 17:55:42 +0530

    Update: UI design for Company Popup and Contact Popup.

--- src/domain/Tickets/OpenNewContactPopup.tsx
+++ src/domain/Tickets/OpenNewContactPopup.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Modal, ModalBody } from 'reactstrap';
 
 export class OpenNewContactPopup extends React.Component<any, any> {
     steps: any;
@@ -15,94 +15,144 @@ export class OpenNewContactPopup extends React.Component<any, any> {
             modal: !this.state.modal,
         });
     }
+    handleClose = () => {
+        this.setState({
+            modal: false,
+        }); 
+    }
 
     render() {
         const { modal } = this.state;
         return (
             <Modal isOpen={modal} toggle={this.toggle} className="modal-container">
-                <ModalHeader toggle={this.toggle}><h4>New Contact</h4>
-                    <span>When someone reaches out to you, they become a contact in your account. You can create companies and associate contacts with them. Learn more.</span></ModalHeader>
-                <ModalBody style={{ height: 'calc(75vh - 110px)', overflowY: 'auto', overflowX: "hidden" }}>
-                    <div className="row">
-                        <div className="col-lg-6 col-md-6 col-sm-12">
-                            <label>Full Name*</label>
-                            <input type="text" className="input-group-text" placeholder="Enter the name of this person" />
+                <ModalBody style={{ height: 'calc(75vh - 50px)', overflowY: 'auto', overflowX: "hidden" }}>
+                    <div className="d-block width-100 contact-popup-container">
+                        <div className="d-block width-100 p-b-20 heading">
+                            <h4 className="d-block"><i className="fa fa-user"></i> New Contact</h4>
+                            <span className="d-block">When someone reaches out to you, they become a contact in your account. You can create companies and associate contacts with them. <a href="#">Learn more.</a></span>
                         </div>
-                        <div className="col-lg-6 col-md-6 col-sm-12">
-                            <label>Title</label>
-                            <input type="text" className="input-group-text" placeholder="Enter a title" />
+                        <div className="d-block width-100 p-t-10 p-b-10 upload-photo">
+                            <div className="d-inline-block upload-icon">
+                                <i className="fa fa-user"></i>
+                            </div>
+                            <div className="d-inline-block v-a-top">
+                                <strong className="d-block">Upload Photo</strong>
+                                <p className="d-block">An image of the person, it's best if it has the same length and height</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="row">
-                        <h3>Atleast one of these fields is mandatory*</h3>
-                        <div className="col-lg-6 col-md-6 col-sm-12">
-                            <label>Email</label>
-                            <input type="text" className="input-group-text" placeholder="Enter a phone email address" />
+                        <div className="row">
+                            <div className="col-lg-6 col-md-6 col-sm-12">
+                                <div className="form-group">
+                                    <label>Full Name*</label>
+                                    <input type="text" className="input-group-text" placeholder="Enter the name of this person" />
+                                </div>
+                            </div>
+                            <div className="col-lg-6 col-md-6 col-sm-12">
+                                <div className="form-group">
+                                    <label>Title</label>
+                                    <input type="text" className="input-group-text" placeholder="Enter a title" />
+                                </div>
+                            </div>
                         </div>
-                        <div className="col-lg-6 col-md-6 col-sm-12">
-                            <label>Alternate Email</label>
-                            <input type="text" className="input-group-text" placeholder="Enter a email address" />
+                        <div className="atleast-fields-box">
+                            <h3>Atleast one of these fields is mandatory*</h3>
+                            <div className="row">
+                                <div className="col-lg-6 col-md-6 col-sm-12">
+                                    <div className="form-group">
+                                        <label>Email</label>
+                                        <input type="text" className="input-group-text" placeholder="Enter a phone email address" />
+                                    </div>
+                                </div>
+                                <div className="col-lg-6 col-md-6 col-sm-12">
+                                    <div className="form-group">
+                                        <label>Alternate Email</label>
+                                        <input type="text" className="input-group-text" placeholder="Enter a email address" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-lg-6 col-md-6 col-sm-12">
+                                    <div className="form-group">
+                                        <label>Work Phone</label>
+                                        <input type="text" className="input-group-text" placeholder="Enter a phone number" />
+                                    </div>
+                                </div>
+                                <div className="col-lg-6 col-md-6 col-sm-12">
+                                    <div className="form-group">
+                                        <label>Mobile Phone</label>
+                                        <input type="text" className="input-group-text" placeholder="Enter a phone number" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-lg-6 col-md-6 col-sm-12">
+                                    <div className="form-group">
+                                        <label>Twitter</label>
+                                        <input type="text" className="input-group-text" placeholder="Enter a Twitter ID" />
+                                    </div>
+                                </div>
+                                <div className="col-lg-6 col-md-6 col-sm-12">
+                                    <div className="form-group">
+                                        <label>Unique external Id</label>
+                                        <input type="text" className="input-group-text" placeholder="Enter the contact’s unique ID" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                       
+                        <div className="row">
+                            <div className="col-lg-12 col-md-12 col-sm-12">
+                                <div className="form-group">
+                                    <label>Company</label>
+                                    <input type="text" className="input-group-text" placeholder="" />
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-6 col-md-6 col-sm-12">
-                            <label>Work Phone</label>
-                            <input type="text" className="input-group-text" placeholder="Enter a phone number" />
+                        <div className="row">
+                            <div className="col-lg-12 col-md-12 col-sm-12">
+                                <div className="form-group">
+                                    <label>Address</label>
+                                    <textarea className="input-group-text" rows={3} placeholder="Enter the address of this person" />
+                                </div>
+                            </div>
                         </div>
-                        <div className="col-lg-6 col-md-6 col-sm-12">
-                            <label>Mobile Phone</label>
-                            <input type="text" className="input-group-text" placeholder="Enter a phone number" />
+                        <div className="row">
+                            <div className="col-lg-6 col-md-6 col-sm-12">
+                                <div className="form-group">
+                                    <label>Time Zone</label>
+                                    <input type="date" className="input-group-text" placeholder="(GMT +05:30) Chennai" />
+                                </div>
+                            </div>
+                            <div className="col-lg-6 col-md-6 col-sm-12">
+                                <div className="form-group">
+                                    <label>Language</label>
+                                    <input type="text" className="input-group-text" placeholder="English" />
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-6 col-md-6 col-sm-12">
-                            <label>Twitter</label>
-                            <input type="text" className="input-group-text" placeholder="Enter a Twitter ID" />
+                        <div className="row">
+                            <div className="col-lg-6 col-md-6 col-sm-12">
+                                <div className="form-group">
+                                    <label>Tags</label>
+                                    <input type="text" className="input-group-text" placeholder="" />
+                                </div>
+                            </div>
+                            <div className="col-lg-6 col-md-6 col-sm-12">
+                                <div className="form-group">
+                                    <label>About</label>
+                                    <input type="text" className="input-group-text" placeholder="Enter some text" />
+                                </div>
+                            </div>
                         </div>
-                        <div className="col-lg-6 col-md-6 col-sm-12">
-                            <label>Unique external Id</label>
-                            <input type="text" className="input-group-text" placeholder="Enter the contact’s unique ID" />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-12 col-md-12 col-sm-12">
-                            <label>Company</label>
-                            <input type="text" className="input-group-text" placeholder="" />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-12 col-md-12 col-sm-12">
-                            <label>Address</label>
-                            <textarea className="input-group-text" placeholder="Enter the address of this person" />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-6 col-md-6 col-sm-12">
-                            <label>Time Zone</label>
-                            <input type="date" className="input-group-text" placeholder="(GMT +05:30) Chennai" />
-                        </div>
-                        <div className="col-lg-6 col-md-6 col-sm-12">
-                            <label>Language</label>
-                            <input type="text" className="input-group-text" placeholder="English" />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-6 col-md-6 col-sm-12">
-                            <label>Tags</label>
-                            <input type="text" className="input-group-text" placeholder="" />
-                        </div>
-                        <div className="col-lg-6 col-md-6 col-sm-12">
-                            <label>About</label>
-                            <input type="text" className="input-group-text" placeholder="Enter some text" />
+                        <div className="row">
+                            <div className="col-lg-12 col-md-12 col-sm-12">
+                                <div className="d-block text-center p-t-20 contact-popup-buttons">
+                                    <button className="cancel" onClick={this.handleClose}>Cancel</button>
+                                    <button className="save">Save</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </ModalBody>
-                <ModalFooter>
-                    <div className="row">
-                        <button>Cancel</button>
-                        <button>Save</button>
-                    </div>
-                </ModalFooter>
             </Modal>
         );
     }
