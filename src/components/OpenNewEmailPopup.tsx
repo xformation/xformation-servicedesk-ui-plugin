270398bbd34c24e336ee77df7a40b58ac1a247f8
dipti96 <diptippatel96@gmail.com>
2020-09-03 14:43:44 +0530

    add validation in email popup

--- src/components/OpenNewEmailPopup.tsx
+++ src/components/OpenNewEmailPopup.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
+import { Fragment } from 'react';
 import { Modal, ModalBody } from 'reactstrap';
 import { CustomTextbox } from './CustomTextbox';
+import { Customselectbox } from './Customselectbox';
+// import SearchableDropdown from 'react-native-searchable-dropdown';
+
 
 export class OpenNewEmailPopup extends React.Component<any, any> {
     steps: any;
@@ -8,15 +12,14 @@ export class OpenNewEmailPopup extends React.Component<any, any> {
         super(props);
         this.state = {
             modal: false,
-            companyName: '',
+            from: '',
+            to: '',
+            subject: '',
             description: '',
-            notes: '',
-            company: '',
-            healthCare: '',
-            accountTier: '',
-            renewalDate: '',
-            industry: '',
-            isSubmitted: false
+            priority: '',
+            status: '',
+            tags: '',
+            isSubmitted: false,
         };
     }
 
@@ -37,17 +40,16 @@ export class OpenNewEmailPopup extends React.Component<any, any> {
             isSubmitted: true
         });
         const errorData = this.validate(true);
-        if (errorData.companyName.isValid && errorData.description.isValid && errorData.notes.isValid && errorData.healthCare.isValid && errorData.company.isValid && errorData.accountTier.isValid && errorData.renewalDate.isValid && errorData.industry.isValid) {
-            const { companyName, description, notes, company, healthCare, accountTier, renewalDate, industry } = this.state;
+        if (errorData.from.isValid && errorData.to.isValid && errorData.subject.isValid && errorData.description.isValid && errorData.priority.isValid && errorData.status.isValid && errorData.tags.isValid) {
+            const { from, to, subject, description, priority, status, tags } = this.state;
             const sendData = {
-                companyName,
+                from,
+                to,
+                subject,
                 description,
-                notes,
-                company,
-                healthCare,
-                accountTier,
-                renewalDate,
-                industry,
+                priority,
+                status,
+                tags,
             };
             console.log(sendData);
         }
@@ -59,21 +61,32 @@ export class OpenNewEmailPopup extends React.Component<any, any> {
             message: ""
         };
         const retData = {
-            companyName: validObj,
+            from: validObj,
+            to: validObj,
+            subject: validObj,
             description: validObj,
-            notes: validObj,
-            company: validObj,
-            healthCare: validObj,
-            accountTier: validObj,
-            renewalDate: validObj,
-            industry: validObj,
+            priority: validObj,
+            status: validObj,
+            tags: validObj,
         };
         if (isSubmitted) {
-            const { companyName, description, notes, company, healthCare, accountTier, renewalDate, industry } = this.state;
-            if (!companyName) {
-                retData.companyName = {
+            const { from, to, subject, description, priority, status, tags } = this.state;
+            if (!from) {
+                retData.from = {
                     isValid: false,
-                    message: "Company Name is required"
+                    message: "Email Sender name is required"
+                };
+            }
+            if (!to) {
+                retData.to = {
+                    isValid: false,
+                    message: "Email Forworder name is required"
+                };
+            }
+            if (!subject) {
+                retData.subject = {
+                    isValid: false,
+                    message: "Subject is required"
                 };
             }
             if (!description) {
@@ -82,40 +95,22 @@ export class OpenNewEmailPopup extends React.Component<any, any> {
                     message: "Description is required"
                 };
             }
-            if (!notes) {
-                retData.notes = {
+            if (!priority) {
+                retData.priority = {
                     isValid: false,
-                    message: "Notes is required"
+                    message: "Priority is required"
                 };
             }
-            if (!company) {
-                retData.company = {
+            if (!status) {
+                retData.status = {
                     isValid: false,
-                    message: "Company is required"
+                    message: "Status is required"
                 };
             }
-            if (!healthCare) {
-                retData.healthCare = {
+            if (!tags) {
+                retData.tags = {
                     isValid: false,
-                    message: "Health Care is required"
-                };
-            }
-            if (!accountTier) {
-                retData.accountTier = {
-                    isValid: false,
-                    message: "Account Tier Name is required"
-                };
-            }
-            if (!renewalDate) {
-                retData.renewalDate = {
-                    isValid: false,
-                    message: "Renewal Date is required"
-                };
-            }
-            if (!industry) {
-                retData.industry = {
-                    isValid: false,
-                    message: "Industry Name is required"
+                    message: "Tags is required"
                 };
             }
         }
@@ -130,78 +125,67 @@ export class OpenNewEmailPopup extends React.Component<any, any> {
     };
 
     render() {
-        const { modal, companyName, description, notes, company, healthCare, accountTier, renewalDate, industry, isSubmitted } = this.state;
+        const { modal, from, to, subject, description, priority, status, tags, isSubmitted } = this.state;
         const errorData = this.validate(isSubmitted);
         return (
             <Modal isOpen={modal} toggle={this.toggle} className="modal-container">
                 <ModalBody style={{ height: 'calc(75vh - 50px)', overflowY: 'auto', overflowX: "hidden" }}>
                     <div className="d-block width-100 contact-popup-container">
                         <div className="d-block width-100 p-b-20 heading">
-                            <h4 className="d-block"><i className="fa fa-building"></i>New Email</h4>
-                            <span className="d-block">When someone reaches out to you, they become a contact in your account. You can create companies and associate contacts with them. <a href="#">Learn more.</a></span>
-                        </div>
-                        <div className="d-block width-100 p-t-10 p-b-10 upload-photo">
-                            <div className="d-inline-block upload-icon">
-                                <i className="fa fa-building"></i>
-                            </div>
-                            <div className="d-inline-block v-a-top">
-                                <strong className="d-block">Upload Logo</strong>
-                                <p className="d-block">An image of the person, it's best if it has the same length and height</p>
-                            </div>
+                            <h4 className="d-block"><i className="fa fa-building"></i>Send an Email</h4>
+                            <span className="d-block">When you hit send, the Contact will receive an email and a Ticket will be associated with Them</span>
                         </div>
                         <div className="row">
-                            <div className="col-lg-6 col-md-6 col-sm-12">
+                            <div className="col-lg-12 col-md-12 col-sm-12">
                                 <div className="form-group">
-                                    <label htmlFor="companyName">Company Name*</label>
-                                    <CustomTextbox containerClass="form-group-inner" inputClass="form-control" htmlFor="companyName" id="companyName" placeholder="" name="companyName" value={companyName} onChange={this.handleStateChange} isValid={errorData.companyName.isValid} message={errorData.companyName.message} />
-                                </div>
-                            </div>
-                            <div className="col-lg-6 col-md-6 col-sm-12">
-                                <div className="form-group">
-                                    <label htmlFor="description">Description</label>
-                                    <CustomTextbox containerClass="form-group-inner" inputClass="form-control" htmlFor="description" id="description" placeholder="Write something that describe this company" name="description" value={description} onChange={this.handleStateChange} isValid={errorData.description.isValid} message={errorData.description.message} />
+                                    <label htmlFor="from">From</label>
+                                    {/* <Customselectbox containerClass="form-group-inner" inputClass="form-control" htmlFor="from" id="from" name="from" value={from} arrayData={{ 0: 'abc@a.com', 1: 'def@b.com', 2: 'ghi@a.com' }} onChange={this.handleStateChange} isValid={errorData.from.isValid} message={errorData.from.message} /> */}
+                                    <CustomTextbox containerClass="form-group-inner" inputClass="form-control" htmlFor="from" id="from" placeholder="RK Fabrication Company (support@ramkaumr1578.maxamis.com)" name="from" value={from} onChange={this.handleStateChange} isValid={errorData.from.isValid} message={errorData.from.message} />
                                 </div>
                             </div>
                         </div>
                         <div className="row">
-                            <div className="col-lg-6 col-md-6 col-sm-12">
+                            <div className="col-lg-12 col-md-12 col-sm-12">
                                 <div className="form-group">
-                                    <label htmlFor="notes">Notes</label>
-                                    <CustomTextbox containerClass="form-group-inner" inputClass="form-control" htmlFor="notes" id="notes" placeholder="Add notes about this company - make something about a recent deal, etc." name="notes" value={notes} onChange={this.handleStateChange} isValid={errorData.notes.isValid} message={errorData.notes.message} />
-                                </div>
-                            </div>
-                            <div className="col-lg-6 col-md-6 col-sm-12">
-                                <div className="form-group">
-                                    <label htmlFor="company">Company</label>
-                                    <CustomTextbox containerClass="form-group-inner" inputClass="form-control" htmlFor="company" id="company" placeholder="eg: My company1.com, mycompany2.com" name="company" value={company} onChange={this.handleStateChange} isValid={errorData.company.isValid} message={errorData.company.message} />
+                                    <label htmlFor="description">To*</label>
+                                    <CustomTextbox containerClass="form-group-inner" inputClass="form-control" htmlFor="to" id="to" placeholder="" name="to" value={to} onChange={this.handleStateChange} isValid={errorData.to.isValid} message={errorData.to.message} />
+                                    {/* <Customselectbox containerClass="form-group-inner" inputClass="form-control" htmlFor="to" id="to" name="to" value={to} arrayData={{ 0: 'abc@a.com', 1: 'def@b.com', 2: 'ghi@a.com' }} onChange={this.handleStateChange} isValid={errorData.to.isValid} message={errorData.to.message} /> */}
                                 </div>
                             </div>
                         </div>
                         <div className="row">
-                            <div className="col-lg-6 col-md-6 col-sm-12">
+                            <div className="col-lg-12 col-md-12 col-sm-12">
                                 <div className="form-group">
-                                    <label htmlFor="healthCare">Health Score</label>
-                                    <CustomTextbox containerClass="form-group-inner" inputClass="form-control" htmlFor="healthCare" id="healthCare" placeholder="Any" name="healthCare" value={healthCare} onChange={this.handleStateChange} isValid={errorData.healthCare.isValid} message={errorData.healthCare.message} />
-                                </div>
-                            </div>
-                            <div className="col-lg-6 col-md-6 col-sm-12">
-                                <div className="form-group">
-                                    <label htmlFor="accountTier">Account tier</label>
-                                    <CustomTextbox containerClass="form-group-inner" inputClass="form-control" htmlFor="accountTier" id="accountTier" placeholder="Any" name="accountTier" value={accountTier} onChange={this.handleStateChange} isValid={errorData.accountTier.isValid} message={errorData.accountTier.message} />
+                                    <label htmlFor="subject">Subject*</label>
+                                    <CustomTextbox containerClass="form-group-inner" inputClass="form-control" htmlFor="subject" id="subject" placeholder="" name="subject" value={subject} onChange={this.handleStateChange} isValid={errorData.subject.isValid} message={errorData.subject.message} />
                                 </div>
                             </div>
                         </div>
                         <div className="row">
-                            <div className="col-lg-6 col-md-6 col-sm-12">
+                            <div className="col-lg-12 col-md-12 col-sm-12">
                                 <div className="form-group">
-                                    <label htmlFor="renewalDate">Renewal Date</label>
-                                    <CustomTextbox containerClass="form-group-inner" type="date" inputClass="form-control" htmlFor="renewalDate" id="renewalDate" placeholder="Any" name="renewalDate" value={renewalDate} onChange={this.handleStateChange} isValid={errorData.renewalDate.isValid} message={errorData.renewalDate.message} />
+                                    <label htmlFor="description">Description*</label>
+                                    <CustomTextbox containerClass="form-group-inner" rows="2" inputClass="form-control" htmlFor="description" id="description" name="description" value={description} onChange={this.handleStateChange} isValid={errorData.description.isValid} message={errorData.description.message} />
                                 </div>
                             </div>
-                            <div className="col-lg-6 col-md-6 col-sm-12">
+                        </div>
+                        <div className="row">
+                            <div className="col-lg-4 col-md-4 col-sm-12">
                                 <div className="form-group">
-                                    <label htmlFor="industry">Industry</label>
-                                    <CustomTextbox containerClass="form-group-inner" inputClass="form-control" htmlFor="industry" id="industry" placeholder="Any" name="industry" value={industry} onChange={this.handleStateChange} isValid={errorData.industry.isValid} message={errorData.industry.message} />
+                                    <label htmlFor="priority">Priority*</label>
+                                    <Customselectbox containerClass="form-group-inner" inputClass="form-control" htmlFor="priority" id="priority" name="priority" value={priority} arrayData={{ 0: 'Low', 1: 'High', 2: 'Medium' }} onChange={this.handleStateChange} isValid={errorData.priority.isValid} message={errorData.priority.message} />
+                                </div>
+                            </div>
+                            <div className="col-lg-4 col-md-4 col-sm-12">
+                                <div className="form-group">
+                                    <label htmlFor="status">Status*</label>
+                                    <Customselectbox containerClass="form-group-inner" inputClass="form-control" htmlFor="status" id="status" name="status" value={status} arrayData={{ 0: 'Closed', 1: 'Open' }} onChange={this.handleStateChange} isValid={errorData.status.isValid} message={errorData.status.message} />
+                                </div>
+                            </div>
+                            <div className="col-lg-4 col-md-4 col-sm-12">
+                                <div className="form-group">
+                                    <label htmlFor="tags">Tags</label>
+                                    <CustomTextbox containerClass="form-group-inner" inputClass="form-control" htmlFor="tags" id="tags" name="tags" value={tags} onChange={this.handleStateChange} isValid={errorData.tags.isValid} message={errorData.tags.message} />
                                 </div>
                             </div>
                         </div>
@@ -215,7 +199,7 @@ export class OpenNewEmailPopup extends React.Component<any, any> {
                         </div>
                     </div>
                 </ModalBody>
-            </Modal>
+            </Modal >
         );
     }
 }
