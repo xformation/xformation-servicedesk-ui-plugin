d1e22653d2e7005a8160c92cabeb6f62183fe33b
dipti96 <diptippatel96@gmail.com>
2020-09-12 16:47:07 +0530

    add chart in chart page

--- src/domain/Charts/index.tsx
+++ src/domain/Charts/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Breadcrumbs } from '../../components/Breadcrumbs';
 import { Link } from 'react-router-dom';
 import { config } from '../../config';
+import { Bar } from 'react-chartjs-2';
 
 export class Charts extends React.Component<any, any> {
     breadCrumbs: any;
@@ -21,6 +22,70 @@ export class Charts extends React.Component<any, any> {
         ];
     }
 
+    barChart1Data = {
+        labels: ['Email', 'Widget', 'Chat'],
+        datasets: [
+            {
+                type: 'bar',
+                label: '',
+                backgroundColor: 'rgba(31, 120, 180, 1)',
+                borderColor: 'rgba(31, 120, 180, 1)',
+                borderWidth: 1,
+                hoverBackgroundColor: 'rgba(31, 120, 180, 1)',
+                hoverBorderColor: 'rgba(31, 120, 180, 1)',
+                data: [100, 380, 200]
+            }
+        ]
+    };
+
+    barChart2Data = {
+        labels: ['Low', 'Medium', 'High'],
+        datasets: [
+            {
+                type: 'bar',
+                label: '',
+                backgroundColor: 'rgba(31, 120, 180, 1)',
+                borderColor: 'rgba(31, 120, 180, 1)',
+                borderWidth: 1,
+                hoverBackgroundColor: 'rgba(31, 120, 180, 1)',
+                hoverBorderColor: 'rgba(31, 120, 180, 1)',
+                data: [280, 180, 350]
+            }
+        ]
+    };
+
+    barChart3Data = {
+        labels: ['Open', 'Inprogress', 'Closed'],
+        datasets: [
+            {
+                type: 'bar',
+                label: '',
+                backgroundColor: 'rgba(31, 120, 180, 1)',
+                borderColor: 'rgba(31, 120, 180, 1)',
+                borderWidth: 1,
+                hoverBackgroundColor: 'rgba(31, 120, 180, 1)',
+                hoverBorderColor: 'rgba(31, 120, 180, 1)',
+                data: [390, 270, 200]
+            }
+        ]
+    };
+
+    barChart4Data = {
+        labels: ['Question', 'Issue'],
+        datasets: [
+            {
+                type: 'bar',
+                label: '',
+                backgroundColor: 'rgba(31, 120, 180, 1)',
+                borderColor: 'rgba(31, 120, 180, 1)',
+                borderWidth: 1,
+                hoverBackgroundColor: 'rgba(31, 120, 180, 1)',
+                hoverBorderColor: 'rgba(31, 120, 180, 1)',
+                data: [320, 150]
+            }
+        ]
+    };
+
     render() {
         return (
             <div className="servicedesk-dashboard-container">
@@ -36,7 +101,7 @@ export class Charts extends React.Component<any, any> {
                             <div className="col-lg-4 col-md-4 col-sm-12 text-right">
                                 <Link to={`${config.basePath}/reporthelpdesh`} className="blue-button m-r-0 m-b-0 min-width-inherit width-auto create-btn">
                                     Back
-                                </Link>                                
+                                </Link>
                             </div>
                         </div>
                     </div>
@@ -44,16 +109,152 @@ export class Charts extends React.Component<any, any> {
                         <div className="charts-boxs">
                             <div className="row">
                                 <div className="col-lg-6 col-md-6 col-sm-12">
-                                    <div className="d-block width-100 chart-box"></div>
+                                    <div className="d-block width-100 chart-box">
+                                        <Bar
+                                            data={this.barChart1Data}
+                                            options={{
+                                                maintainAspectRatio: false,
+                                                legend: {
+                                                    display: false,
+                                                    position: 'right'
+                                                },
+                                                scales: {
+                                                    yAxes: [{
+                                                        // gridLines: {
+                                                        //     color: "rgba(240, 243, 247, 1)",
+                                                        // },
+                                                        ticks: {
+                                                            fontColor: 'rgba(169, 185, 198, 1)',
+                                                            fontSize: 12,
+                                                            beginAtZero: true,
+                                                            min: 0,
+                                                            max: 400
+                                                        },
+                                                    }],
+                                                    xAxes: [{
+                                                        gridLines: {
+                                                            color: "rgba(240, 243, 247, 1)",
+                                                        },
+                                                        ticks: {
+                                                            fontColor: 'rgba(169, 185, 198, 1)',
+                                                            fontSize: 12
+                                                        },
+                                                    }]
+                                                }
+                                            }}
+                                        />
+                                    </div>
                                 </div>
                                 <div className="col-lg-6 col-md-6 col-sm-12">
-                                    <div className="d-block width-100 chart-box"></div>
+                                    <div className="d-block width-100 chart-box">
+                                        <Bar
+                                            data={this.barChart2Data}
+                                            options={{
+                                                maintainAspectRatio: false,
+                                                legend: {
+                                                    display: false,
+                                                    position: 'right'
+                                                },
+                                                scales: {
+                                                    yAxes: [{
+                                                        // gridLines: {
+                                                        //     color: "rgba(240, 243, 247, 1)",
+                                                        // },
+                                                        ticks: {
+                                                            fontColor: 'rgba(169, 185, 198, 1)',
+                                                            fontSize: 12,
+                                                            beginAtZero: true,
+                                                            min: 0,
+                                                            max: 400
+                                                        },
+                                                    }],
+                                                    xAxes: [{
+                                                        gridLines: {
+                                                            color: "rgba(240, 243, 247, 1)",
+                                                        },
+                                                        ticks: {
+                                                            fontColor: 'rgba(169, 185, 198, 1)',
+                                                            fontSize: 12
+                                                        },
+                                                    }]
+                                                }
+                                            }}
+                                        />
+                                    </div>
                                 </div>
                                 <div className="col-lg-6 col-md-6 col-sm-12">
-                                    <div className="d-block width-100 chart-box"></div>
+                                    <div className="d-block width-100 chart-box">
+                                        <Bar
+                                            data={this.barChart3Data}
+                                            options={{
+                                                maintainAspectRatio: false,
+                                                legend: {
+                                                    display: false,
+                                                    position: 'right'
+                                                },
+                                                scales: {
+                                                    yAxes: [{
+                                                        // gridLines: {
+                                                        //     color: "rgba(240, 243, 247, 1)",
+                                                        // },
+                                                        ticks: {
+                                                            fontColor: 'rgba(169, 185, 198, 1)',
+                                                            fontSize: 12,
+                                                            beginAtZero: true,
+                                                            min: 0,
+                                                            max: 400
+                                                        },
+                                                    }],
+                                                    xAxes: [{
+                                                        gridLines: {
+                                                            color: "rgba(240, 243, 247, 1)",
+                                                        },
+                                                        ticks: {
+                                                            fontColor: 'rgba(169, 185, 198, 1)',
+                                                            fontSize: 12
+                                                        },
+                                                    }]
+                                                }
+                                            }}
+                                        />
+                                    </div>
                                 </div>
                                 <div className="col-lg-6 col-md-6 col-sm-12">
-                                    <div className="d-block width-100 chart-box"></div>
+                                    <div className="d-block width-100 chart-box">
+                                        <Bar
+                                            data={this.barChart3Data}
+                                            options={{
+                                                maintainAspectRatio: false,
+                                                legend: {
+                                                    display: false,
+                                                    position: 'right'
+                                                },
+                                                scales: {
+                                                    yAxes: [{
+                                                        // gridLines: {
+                                                        //     color: "rgba(240, 243, 247, 1)",
+                                                        // },
+                                                        ticks: {
+                                                            fontColor: 'rgba(169, 185, 198, 1)',
+                                                            fontSize: 12,
+                                                            beginAtZero: true,
+                                                            min: 0,
+                                                            max: 400
+                                                        },
+                                                    }],
+                                                    xAxes: [{
+                                                        gridLines: {
+                                                            color: "rgba(240, 243, 247, 1)",
+                                                        },
+                                                        ticks: {
+                                                            fontColor: 'rgba(169, 185, 198, 1)',
+                                                            fontSize: 12
+                                                        },
+                                                    }]
+                                                }
+                                            }}
+                                        />
+                                    </div>
                                 </div>
                             </div>
                         </div>
